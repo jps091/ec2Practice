a8ace3082760e41fe0c1f38fa984d3681ff6f074
jaepil <jaepill94@gmail.com>
2024-07-18 10:31:22 +0900

    health check2

--- src/main/java/lecture/ec2/controller/UserController.java
+++ src/main/java/lecture/ec2/controller/UserController.java
@@ -1,6 +1,8 @@
 package lecture.ec2.controller;
 
 import lecture.ec2.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,18 @@ public class UserController {
     public String Call(
     ){
         return "ec2 ";
+    }
+
+    public class OtherServerHealthIndicator {
+
+        @GetMapping("/health")
+        public ResponseEntity<String> health() {
+            int httpStatus = 200; // 실제 로직으로 헬스 체크
+            if (httpStatus == HttpStatus.OK.value()) {
+                return new ResponseEntity<>("Healthy", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Unhealthy", HttpStatus.SERVICE_UNAVAILABLE);
+        }
     }
 
 
